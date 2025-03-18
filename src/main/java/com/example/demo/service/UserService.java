ed90ee15ea636d0e0533b8ad5a1cd4e167d78a1a
AnjanaTharanga24 <anjana24myprof@gmail.com>
2025-03-18 10:08:14 +0000

    create login function

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,8 +4,10 @@ import com.example.demo.dto.request.LoginRequest;
 import com.example.demo.dto.request.UserRequest;
 import com.example.demo.dto.response.LoginResponse;
 import com.example.demo.dto.response.UserResponse;
+import com.example.demo.exception.AllReadyExistsException;
+import com.example.demo.exception.NotFoundException;
 
 public interface UserService {
-    UserResponse registerUser(UserRequest userRequest);
-    LoginResponse loginUser(LoginRequest loginRequest);
+    UserResponse registerUser(UserRequest userRequest) throws AllReadyExistsException;
+    LoginResponse loginUser(LoginRequest loginRequest) throws NotFoundException;
 }
